ef7457752d9e98f8c51002b9e1d3644e7e4fc3b8
VaishaliPandiyen <81515185+VaishaliPandiyen@users.noreply.github.com>
2022-12-24 17:17:28 +0000

    Set up a blog (circular imports issue should be resolved in _init_.py)

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,6 +1,9 @@
 
-from blog import routes
 from flask import Flask
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '50a3b7cc91918d658cddc43d7ad46af1411a0bad2daf66c0'
+
+# this <from blog import routes> should be here in the last line (or after app.config) as the order of imports in the _init_.py file is important. The last thing this script should do is import the routes.py file.
+from blog import routes
+# end of exlaination
